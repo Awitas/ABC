434fb10eaf9edbb193c4b6fbf11b7ad0f24182a7
Awitas <Awitas@gmail.com>
2016-01-26 11:58:01 -0600

    Create ABC.js

---
+++ ABC.js
@@ -0,0 +1,13 @@
+if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
+    script = document.createElement( 'script' );
+   script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'; 
+    script.onload=fillOrder;
+    document.body.appendChild(script);
+} 
+else {
+    fillOrder();
+}
+ 
+function fillOrder() {
+    alert("testing")
+}
